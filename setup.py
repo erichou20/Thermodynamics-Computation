3a9f1a7d869cf3848cc1dcc50ad3de1b1bce3539
ehou1 <eric-hou@uiowa.edu>
2024-08-23 11:46:03 -0500

    Cleaning

--- setup.py
+++ setup.py
@@ -11,4 +11,11 @@ setup(
     package=find_packages(where='Thermodynamics-Computation'),
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url= "https://github.com/erichou20/Thermodynamics-Computation",
+    author="erichou20",
+    author_email="houeric6@gmail.com",
+    license="MIT",
+    classifiers=[],
+    install_requires=[],
+    python_requires=">=3.5",
 )
